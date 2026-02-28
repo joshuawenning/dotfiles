85576184d849df8acf5bb470e61f239afc799b32
Joshua Wenning <joshuawenning@gmail.com>
2026-02-28 10:17:51 -0500

    Add Alacritty and tmux

--- setup-symlinks.sh
+++ setup-symlinks.sh
@@ -9,6 +9,9 @@ ln -s ~/Documents/Projects/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/Documents/Projects/dotfiles/gitignore ~/.gitignore
 ln -s ~/Documents/Projects/dotfiles/vimrc ~/.vimrc
 ln -s ~/Documents/Projects/dotfiles/zshrc ~/.zshrc
+mkdir -p ~/.config/alacritty ~/.config/tmux
+ln -s ~/Documents/Projects/dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -s ~/Documents/Projects/dotfiles/tmux.conf ~/.config/tmux/tmux.conf
 
 # Initialize settings
 source ~/.zshrc
